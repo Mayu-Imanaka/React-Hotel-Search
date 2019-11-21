4e2c8b7de7f3e92f33f699c8ba652dc75bdd26cc
Mayu Imanaka <imamayu.1215@gmail.com>
2019-11-21 15:41:35 -0800

    add:HotelRepository.js

--- src/domain/HotelRepository.js
+++ src/domain/HotelRepository.js
@@ -2,13 +2,17 @@ import Rakuten from '../lib/Rakuten';
 
 const RAKUTEN_APP_ID = '1094848202921756224';
 
-export const searchHotelByLocation = location =>
-const params = {
+// eslint-disable-next-line import/prefer-default-export
+export const searchHotelByLocation = (location) => {
+  const params = {
     applicationId: RAKUTEN_APP_ID,
     detumType: 1,
     latitude: location.lat,
     longitude: location.lng,
+  };
+  return Rakuten.Travel.simpleHotelSearch(params)
+    .then((result) => {
+      // eslint-disable-next-line no-console
+      console.log(result);
+    });
 };
-    return Rakuten.Travel.simpleHotelSearch(params);
-    .then(result);
-;
